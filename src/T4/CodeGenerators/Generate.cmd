b519c22bd2b3478f55a485c0efa365a56aad8b71
Paul Deen <paul.deen@dutchworkz.nl>
2021-11-08 15:19:58 +0100

    Minor: Use global tool for generation of code generators

--- src/T4/CodeGenerators/Generate.cmd
+++ src/T4/CodeGenerators/Generate.cmd
@@ -1,4 +1,2 @@
-C:
-cd ..\..\T4.Plus.Cmd\bin\Debug\net5.0
-dotnet TextTemplateTransformationFramework.T4.Plus.Cmd.dll source -f "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\Templates\T4CSharpCodeGenerator.template" -o "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\T4CSharpCodeGenerator.cs" -Parameters $T4Plus.BasePath:C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\
+t4plus source -f "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\Templates\T4CSharpCodeGenerator.template" -o "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\T4CSharpCodeGenerator.cs" -Parameters $T4Plus.BasePath:C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\
 pause
