d5efc0a908d3277dd38fd7e6d075ed4b7ac8eb97
Paul Deen <paul.deen@dutchworkz.nl>
2021-11-10 09:02:22 +0100

    Simplified batch files, and fixed paths

--- src/T4.Plus/CodeGenerators/Generate.cmd
+++ src/T4.Plus/CodeGenerators/Generate.cmd
@@ -1,2 +1,2 @@
-t4plus source -f "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4.Plus\CodeGenerators\Templates\T4PlusCSharpCodeGenerator.template" -o "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4.Plus\CodeGenerators\T4PlusCSharpCodeGenerator.cs" -Parameters $T4Plus.BasePath:C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\
+t4plus source -f "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4.Plus\CodeGenerators\Templates\T4PlusCSharpCodeGenerator.template" -o "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4.Plus\CodeGenerators\T4PlusCSharpCodeGenerator.cs" $T4Plus.BasePath:C:\Git\TextTemplateTransformationFramework\Src\
 pause
--- src/T4/CodeGenerators/Generate.cmd
+++ src/T4/CodeGenerators/Generate.cmd
@@ -1,2 +1,2 @@
-t4plus source -f "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\Templates\T4CSharpCodeGenerator.template" -o "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\T4CSharpCodeGenerator.cs" -Parameters $T4Plus.BasePath:C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\
+t4plus source -f "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\Templates\T4CSharpCodeGenerator.template" -o "C:\Project\Prive\GenericCodeGen\TextTemplateTransformationFramework\T4\CodeGenerators\T4CSharpCodeGenerator.cs" $T4Plus.BasePath:C:\Git\TextTemplateTransformationFramework\Src\
 pause
