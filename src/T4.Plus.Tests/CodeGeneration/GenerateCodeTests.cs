1b26ea7d167e5c90e069462183618c47ef0a4b9d
Paul Deen <paul.deen@dutchworkz.nl>
2022-01-23 08:43:09 +0100

    Added helper to generate templates, which only needs code generation settings and no additional code. (except for a one-liner; GenerateCode.For<T>(settings))

---
+++ src/T4.Plus.Tests/CodeGeneration/GenerateCodeTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using TextTemplateTransformationFramework.Common;
+using TextTemplateTransformationFramework.Common.Default.TemplateTokens.RenderTokens;
+using TextTemplateTransformationFramework.Runtime.CodeGeneration;
+using TextTemplateTransformationFramework.T4.Plus.CodeGenerators;
+using Xunit;
+
+namespace TextTemplateTransformationFramework.T4.Plus.Tests.CodeGeneration
+{
+    [ExcludeFromCodeCoverage]
+    public class GenerateCodeTests
+    {
+        [Fact]
+        public void Can_Generate_Code()
+        {
+            // Act
+            var result = GenerateCode.For<T4PlusCSharp>(new CodeGenerationSettings(null, false, true));
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [ExcludeFromCodeCoverage]
+        private class T4PlusCSharp : ICodeGenerationProvider
+        {
+            public bool GenerateMultipleFiles { get; private set; }
+
+            public string BasePath { get; private set; }
+
+            public string Path => null;
+
+            public string DefaultFileName => "Generated.cs";
+
+            public bool RecurseOnDeleteGeneratedFiles => false;
+
+            public string LastGeneratedFilesFileName => "Generated.cs";
+
+            public Action AdditionalActionDelegate => null;
+
+            public object CreateAdditionalParameters()
+            {
+                return null;
+            }
+
+            public object CreateGenerator()
+            {
+                return new T4PlusCSharpCodeGenerator();
+            }
+
+            public object CreateModel()
+            {
+                return new[] { new RenderCodeToken<TokenParserState>(SectionContext.FromCurrentMode(Mode.CodeRender, new TokenParserState()), "// hello world!") };
+            }
+
+            public void Initialize(bool generateMultipleFiles, string basePath)
+            {
+                GenerateMultipleFiles = generateMultipleFiles;
+                BasePath = basePath;
+            }
+        }
+    }
+}
