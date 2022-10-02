4119351e14b840c1f4e73dabfa4fbdb8ba248c08
Paul Deen <paul.deen@dutchworkz.nl>
2022-10-02 11:14:30 +0200

    Added second overload for constructor on CodeGenerationSettings, and added conventience methods to switch from code generation fo scaffolding and back

--- src/Runtime/CodeGeneration/CodeGenerationSettings.cs
+++ src/Runtime/CodeGeneration/CodeGenerationSettings.cs
@@ -2,6 +2,10 @@
 {
     public class CodeGenerationSettings
     {
+        public CodeGenerationSettings(string basePath, bool dryRun) : this(basePath, false, false, dryRun)
+        {
+        }
+
         public CodeGenerationSettings(string basePath, bool generateMultipleFiles, bool skipWhenFileExists, bool dryRun)
         {
             BasePath = basePath;
@@ -14,5 +18,8 @@
         public bool GenerateMultipleFiles { get; }
         public bool SkipWhenFileExists { get; }
         public bool DryRun { get; }
+
+        public CodeGenerationSettings ForGeneration() => new(BasePath, GenerateMultipleFiles, false, DryRun);
+        public CodeGenerationSettings ForScaffolding() => new(BasePath, GenerateMultipleFiles, true, DryRun);
     }
 }
