8ee08b494c2be62d2ec4101f73cb354f6bc18af5
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-28 19:53:03 +0200

    Added watch option to Run template and Run code generation provider assembly commands

--- src/Common.Cmd/CommandLineCommands/CommandBase.cs
+++ src/Common.Cmd/CommandLineCommands/CommandBase.cs
@@ -1,7 +1,9 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Runtime.Loader;
+using System.Threading;
 using McMaster.Extensions.CommandLineUtils;
 using TextTemplateTransformationFramework.Common.Cmd.Extensions;
 using TextTemplateTransformationFramework.Common.Contracts;
@@ -97,6 +99,41 @@ namespace TextTemplateTransformationFramework.Common.Cmd.CommandLineCommands
             }
 
             return (true, null, directive);
+        }
+
+        [ExcludeFromCodeCoverage]
+        internal static void Watch(CommandLineApplication app, CommandOption<string> watchOption, string fileName, Action action)
+        {
+            action();
+
+            if (!watchOption.HasValue())
+            {
+                return;
+            }
+
+            if (!File.Exists(fileName))
+            {
+                app.Out.WriteLine($"Error: Could not find file [{fileName}]. Could not watch file for changes.");
+                return;
+            }
+
+            app.Out.WriteLine($"Watching file [{fileName}] for changes...");
+            var previousLastWriteTime = new FileInfo(fileName).LastWriteTime;
+            while (true)
+            {
+                if (!File.Exists(fileName))
+                {
+                    app.Out.WriteLine($"Error: Could not find file [{fileName}]. Could not watch file for changes.");
+                    return;
+                }
+                var currentLastWriteTime = new FileInfo(fileName).LastWriteTime;
+                if (currentLastWriteTime != previousLastWriteTime)
+                {
+                    previousLastWriteTime = currentLastWriteTime;
+                    action();
+                }
+                Thread.Sleep(1000);
+            }
         }
     }
 }
