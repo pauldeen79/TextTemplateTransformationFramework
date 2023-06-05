853bb183431450383e43701437c8d3eea7cba4fb
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-05 20:06:14 +0200

    Added a bunch of unit tests on new commands

---
+++ src/Common.Cmd.Tests/CommandLineCommands/AddTemplateCommandTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using CrossCutting.Common.Testing;
+using FluentAssertions;
+using McMaster.Extensions.CommandLineUtils;
+using Moq;
+using TextTemplateTransformationFramework.Common.Cmd.CommandLineCommands;
+using TextTemplateTransformationFramework.Common.Cmd.Tests.TestFixtures;
+using TextTemplateTransformationFramework.Common.Contracts;
+using Xunit;
+
+namespace TextTemplateTransformationFramework.Common.Cmd.Tests.CommandLineCommands
+{
+    [ExcludeFromCodeCoverage]
+    public class AddTemplateCommandTests
+    {
+        private readonly Mock<IFileContentsProvider> _fileContentsProviderMock = new();
+        private readonly Mock<ITemplateInfoRepository> _templateInfoRepositoryMock = new();
+
+        [Fact]
+        public void Ctor_Throws_On_Null_Argument()
+        {
+            TestHelpers.ConstructorMustThrowArgumentNullException(typeof(AddTemplateCommand));
+        }
+
+        [Fact]
+        public void Initialize_Adds_VersionCommand_To_Application()
+        {
+            // Arrange
+            var app = new CommandLineApplication();
+            var sut = CreateSut();
+
+            // Act
+            sut.Initialize(app);
+
+            // Assert
+            app.Commands.Should().ContainSingle();
+        }
+
+        [Fact]
+        public void Initialize_Throws_On_Null_Argument()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act & Assert
+            sut.Invoking(x => x.Initialize(null)).Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Execute_Without_ShortName_Leads_To_Error()
+        {
+            // Arrange
+            _fileContentsProviderMock.Setup(x => x.FileExists(It.IsAny<string>())).Returns(true);
+
+            // Act
+            var actual = CommandLineCommandHelper.ExecuteCommand(CreateSut, "-f my.template");
+
+            // Assert
+            actual.Should().Be("Error: Shortname is required." + Environment.NewLine);
+        }
+
+        [Fact]
+        public void Execute_With_ShortName_And_Filename_Adds_Template_To_Repository()
+        {
+            // Arrange
+            _fileContentsProviderMock.Setup(x => x.FileExists(It.IsAny<string>())).Returns(true);
+
+            // Act
+            var actual = CommandLineCommandHelper.ExecuteCommand(CreateSut, "-f my.template", "-s mytemplate");
+
+            // Assert
+            actual.Should().Be("Template has been added successfully." + Environment.NewLine);
+            _templateInfoRepositoryMock.Verify(x => x.Add(It.IsAny<TemplateInfo>()), Times.Once);
+        }
+
+        private AddTemplateCommand CreateSut()
+            => new AddTemplateCommand(_fileContentsProviderMock.Object, _templateInfoRepositoryMock.Object);
+    }
+}
