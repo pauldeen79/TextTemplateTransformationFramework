bea510a8f9d45223d8571f179ee075d393e4ce08
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-11 16:21:54 +0200

    Added missing unit tests for DictionaryOfStringObjectExtensions class

---
+++ src/Common.Tests/Extensions/DictionaryOfStringObjectExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using TextTemplateTransformationFramework.Common.Extensions;
+using Xunit;
+
+namespace TextTemplateTransformationFramework.Common.Tests.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public class DictionaryOfStringObjectExtensionsTests
+    {
+        [Fact]
+        public void GetValue_Without_DefaultValue_Returns_Null_When_Key_Is_Not_Found()
+        {
+            // Arrange
+            var sut = new Dictionary<string, object>();
+            sut.Add("Key", "Value");
+
+            // Act
+            var actual = sut.GetValue<string>("WrongKey");
+
+            // Assert
+            actual.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetValue_Without_DefaultValue_Returns_Value_When_Key_Is_Found()
+        {
+            // Arrange
+            var sut = new Dictionary<string, object>();
+            sut.Add("Key", "Value");
+
+            // Act
+            var actual = sut.GetValue<string>("Key");
+
+            // Assert
+            actual.Should().Be("Value");
+        }
+
+        [Fact]
+        public void GetValue_With_DefaultValue_Returns_DefaultValue_When_Key_Is_Not_Found()
+        {
+            // Arrange
+            var sut = new Dictionary<string, object>();
+            sut.Add("Key", "Value");
+
+            // Act
+            var actual = sut.GetValue<string>("WrongKey", "DefaultValue");
+
+            // Assert
+            actual.Should().Be("DefaultValue");
+        }
+
+        [Fact]
+        public void GetValue_With_DefaultValue_Returns_Value_When_Key_Is_Found()
+        {
+            // Arrange
+            var sut = new Dictionary<string, object>();
+            sut.Add("Key", "Value");
+
+            // Act
+            var actual = sut.GetValue<string>("Key", "DefaultValue");
+
+            // Assert
+            actual.Should().Be("Value");
+        }
+    }
+}
