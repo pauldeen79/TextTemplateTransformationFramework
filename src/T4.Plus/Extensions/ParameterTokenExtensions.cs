86eb9b6dfa3f0332625e1bc9eab94bc5aed42256
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-15 20:05:24 +0200

    Removed unused code, no need to test it this way ;-)

--- src/T4.Plus/Extensions/ParameterTokenExtensions.cs
+++ src/T4.Plus/Extensions/ParameterTokenExtensions.cs
@@ -1,35 +1,9 @@
-﻿using TextTemplateTransformationFramework.T4.Plus.Contracts.TemplateTokens;
-using TextTemplateTransformationFramework.T4.Plus.Default.TemplateTokens;
+﻿using TextTemplateTransformationFramework.T4.Plus.Default.TemplateTokens;
 
 namespace TextTemplateTransformationFramework.T4.Plus.Extensions
 {
     public static class ParameterTokenExtensions
     {
-        public static IParameterToken<TState> WithPropertySetter<TState>(this IParameterToken<TState> instance)
-            where TState : class
-            => new ParameterToken<TState>
-            (
-                instance.SectionContext,
-                instance.Name,
-                instance.TypeName,
-                instance.NetCoreCompatible,
-                instance.DefaultValue,
-                instance.DefaultValueIsLiteral,
-                instance.OmitValueAssignment,
-                addPropertySetter: true,
-                componentModelData: new(
-                    browsable: false /*instance.Browsable*/,
-                    readOnly: instance.ReadOnly,
-                    required: instance.Required,
-                    displayName:  instance.DisplayName,
-                    description: instance.Description,
-                    typeNameData: new(
-                        editorAttributeEditorTypeName: instance.EditorAttributeEditorTypeName,
-                        editorAttributeEditorBaseType: instance.EditorAttributeEditorBaseType,
-                        typeConverterTypeName: instance.TypeConverterTypeName),
-                    category: instance.Category)
-            );
-
         public static TextTemplateTransformationFramework.Common.Contracts.TemplateTokens.IParameterToken<TState> WithPropertySetter<TState>(this TextTemplateTransformationFramework.Common.Contracts.TemplateTokens.IParameterToken<TState> instance)
             where TState : class
             => new ParameterToken<TState>
