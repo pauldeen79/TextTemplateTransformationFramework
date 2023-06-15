217ce03dbacdaac353b5280e8b38fc5f869f3d0f
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-15 20:09:17 +0200

    Added missing unit test

---
+++ src/T4.Plus.Tests/Extensions/ParameterTokenExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using TextTemplateTransformationFramework.Common;
+using TextTemplateTransformationFramework.T4.Plus.Default.TemplateTokens;
+using TextTemplateTransformationFramework.T4.Plus.Extensions;
+using Xunit;
+
+namespace TextTemplateTransformationFramework.T4.Plus.Tests.Extensions
+{
+    public class ParameterTokenExtensionsTests
+    {
+        [Fact]
+        public void WithPropertySetter_Returns_Instance_With_PropertySetter_Set_To_True()
+        {
+            // Arrange
+            var sut = new ParameterToken<ParameterTokenExtensionsTests>(SectionContext.FromCurrentMode(0, this), "x", "y", addPropertySetter: false);
+
+            // Act
+            var actual = sut.WithPropertySetter();
+
+            // Assert
+            actual.Should().BeOfType<ParameterToken<ParameterTokenExtensionsTests>>();
+            ((ParameterToken<ParameterTokenExtensionsTests>)actual).AddPropertySetter.Should().BeTrue();
+        }
+    }
+}
