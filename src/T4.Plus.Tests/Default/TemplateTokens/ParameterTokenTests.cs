e9816b90957ffdae80dde355bf81b1b2ff035b34
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-15 19:55:54 +0200

    Added missing tests for simple data transfer classes / entities

---
+++ src/T4.Plus.Tests/Default/TemplateTokens/ParameterTokenTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using FluentAssertions;
+using TextTemplateTransformationFramework.Common;
+using TextTemplateTransformationFramework.T4.Plus.Default.TemplateTokens;
+using Xunit;
+
+namespace TextTemplateTransformationFramework.T4.Plus.Tests.Default.TemplateTokens
+{
+    public class ParameterTokenTests
+    {
+        [Fact]
+        public void Should_Construct_With_Default_Values()
+        {
+            this.Invoking(_ => new ParameterToken<ParameterTokenTests>(SectionContext.FromCurrentMode(1, this), "", "")).Should().NotThrow();
+        }
+
+        [Fact]
+        public void Should_Construct_With_Non_Default_Values()
+        {
+            this.Invoking(_ => new ParameterToken<ParameterTokenTests>(SectionContext.FromCurrentMode(1, this), "", "", componentModelData: new())).Should().NotThrow();
+        }
+
+        [Fact]
+        public void Should_Be_Able_To_Read_All_Properties()
+        {
+            // Arrange
+            var sut = new ParameterToken<ParameterTokenTests>(SectionContext.FromCurrentMode(1, this), "", "");
+
+            // Act & Assert
+            sut.GetType().GetProperties().ToList().ForEach(x => x.Invoking(x => x.GetValue(sut)).Should().NotThrow());
+        }
+    }
+}
