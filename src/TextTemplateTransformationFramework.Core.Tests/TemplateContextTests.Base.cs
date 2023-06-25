fd75664e40d8c9b83d65046624210b6832c69a85
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-25 17:17:11 +0200

    Added unit tests for GetModelFromContextByType method

--- src/TextTemplateTransformationFramework.Core.Tests/TemplateContextTests.Base.cs
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateContextTests.Base.cs
@@ -2,4 +2,11 @@
 
 public partial class TemplateContextTests
 {
+    protected TemplateContext CreateSut()
+    {
+        var rootTemplateContext = new TemplateContext(template: this, parentContext: null, model: 1);
+        var parentTemplateContext = new TemplateContext(template: this, parentContext: rootTemplateContext, model: "test");
+        return new TemplateContext(template: this, parentContext: parentTemplateContext);
+    }
+
 }
---
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateContextTests.GetModelFromContextByType.cs
@@ -0,0 +1,59 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class TemplateContextTests
+{
+    public class GetModelFromContextByType : TemplateContextTests
+    {
+        [Fact]
+        public void Returns_Null_When_Model_Type_Could_Not_Be_Found()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.GetModelFromContextByType<GetModelFromContextByType>();
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Returns_Type_From_Parent_When_Correct_Not_Using_Predicate()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.GetModelFromContextByType<string>();
+
+            // Assert
+            result.Should().Be("test");
+        }
+
+        [Fact]
+        public void Returns_Type_From_Root_When_Correct_Using_Predicate_Returns_True()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.GetModelFromContextByType<int>(_ => true);
+
+            // Assert
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Returns_Type_From_Root_When_Correct_Using_Predicate_Returns_False()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.GetModelFromContextByType<int>(_ => false);
+
+            // Assert
+            result.Should().Be(default(int));
+        }
+    }
+}
