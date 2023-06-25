4bb0e60728bf59d500ede5b9a138a30b8d2f4e4f
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-25 20:00:30 +0200

    Added unit tests for TemplateFileManagerFactory

---
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateFileManagerFactoryTests.Create.cs
@@ -0,0 +1,20 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class TemplateFileManagerFactoryTests
+{
+    public class Create
+    {
+        [Fact]
+        public void Creates_New_TemplateFileManager_Instance()
+        {
+            // Arrange
+            var sut = new TemplateFileManagerFactory();
+
+            // Act
+            var result = sut.Create(TestData.BasePath);
+
+            // Assert
+            result.Should().BeOfType<TemplateFileManager>();
+        }
+    }
+}
