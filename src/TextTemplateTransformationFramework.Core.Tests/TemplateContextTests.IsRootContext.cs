c3b5e520ab5597f61b691063c08503c0ccf9da9b
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-25 17:03:10 +0200

    Added unit tests for RootContext and IsRootContext properties

---
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateContextTests.IsRootContext.cs
@@ -0,0 +1,34 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class TemplateContextTests
+{
+    public class IsRootContext
+    {
+        [Fact]
+        public void Returns_True_When_There_Is_No_ParentContext()
+        {
+            // Arrange
+            var sut = new TemplateContext(template: this, parentContext: null);
+
+            // Act
+            var result = sut.IsRootContext;
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Returns_False_When_There_Is_A_ParentContext()
+        {
+            // Arrange
+            var parentTemplateContext = new TemplateContext(template: this, parentContext: null);
+            var sut = new TemplateContext(template: this, parentContext: parentTemplateContext);
+
+            // Act
+            var result = sut.IsRootContext;
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateContextTests.RootContext.cs
@@ -0,0 +1,49 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class TemplateContextTests
+{
+    public class RootContext
+    {
+        [Fact]
+        public void Returns_Same_Instance_When_There_Is_No_ParentContext()
+        {
+            // Arrange
+            var sut = new TemplateContext(template: this, parentContext: null);
+
+            // Act
+            var result = sut.RootContext;
+
+            // Assert
+            result.RootContext.Should().BeSameAs(sut);
+        }
+
+        [Fact]
+        public void Returns_RootContext_One_Level_Deep()
+        {
+            // Arrange
+            var parentTemplateContext = new TemplateContext(template: this, parentContext: null);
+            var sut = new TemplateContext(template: this, parentContext: parentTemplateContext);
+
+            // Act
+            var result = sut.RootContext;
+
+            // Assert
+            result.RootContext.Should().BeSameAs(parentTemplateContext);
+        }
+
+        [Fact]
+        public void Returns_RootContext_Two_Levels_Deep()
+        {
+            // Arrange
+            var rootTemplateContext = new TemplateContext(template: this, parentContext: null);
+            var parentTemplateContext = new TemplateContext(template: this, parentContext: rootTemplateContext);
+            var sut = new TemplateContext(template: this, parentContext: parentTemplateContext);
+
+            // Act
+            var result = sut.RootContext;
+
+            // Assert
+            result.RootContext.Should().BeSameAs(rootTemplateContext);
+        }
+    }
+}
