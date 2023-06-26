d0217ebaa07dbb0b4fe4c4debdd3370b03b05b79
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-26 12:40:44 +0200

    Minor: Unified filenames of unit tests

---
+++ src/TextTemplateTransformationFramework.Core.Tests/CodeGenerationSettingsTests.Constructor.cs
@@ -0,0 +1,50 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class CodeGenerationSettingsTests
+{
+    public class Constructor
+    {
+        [Fact]
+        public void Creates_Instance_With_BasePath_And_DryRun()
+        {
+            // Act
+            var sut = new CodeGenerationSettings(TestData.BasePath, true);
+
+            // Assert
+            sut.BasePath.Should().Be(TestData.BasePath);
+            sut.DryRun.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Creates_Instance_With_BasePath_And_GenerateMultipleFiles_And_DryRun()
+        {
+            // Act
+            var sut = new CodeGenerationSettings(TestData.BasePath, true, true);
+
+            // Assert
+            sut.BasePath.Should().Be(TestData.BasePath);
+            sut.GenerateMultipleFiles.Should().BeTrue();
+            sut.DryRun.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Creates_Instance_With_BasePath_And_GeneratedMultipleFIles_And_SkipWhenFileExists_And_DryRun()
+        {
+            // Act
+            var sut = new CodeGenerationSettings(TestData.BasePath, true, true, true);
+
+            // Assert
+            sut.BasePath.Should().Be(TestData.BasePath);
+            sut.GenerateMultipleFiles.Should().BeTrue();
+            sut.SkipWhenFileExists.Should().BeTrue();
+            sut.DryRun.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Throws_On_Null_BasePath()
+        {
+            this.Invoking(_ => new CodeGenerationSettings(null!, false))
+                .Should().Throw<ArgumentNullException>().WithParameterName("basePath");
+        }
+    }
+}
