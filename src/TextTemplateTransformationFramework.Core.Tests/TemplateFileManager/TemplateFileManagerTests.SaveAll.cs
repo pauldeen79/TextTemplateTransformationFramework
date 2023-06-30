227dc03597984ecb275aebd04c9797b1f4ec5756
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-30 16:34:03 +0200

    Added test for SaveAll method of TemplateFileManager

--- src/TextTemplateTransformationFramework.Core.Tests/TemplateFileManager/TemplateFileManagerTests.SaveAll.cs
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateFileManager/TemplateFileManagerTests.SaveAll.cs
@@ -4,5 +4,17 @@ public partial class TemplateFileManagerTests
 {
     public class SaveAll : TemplateFileManagerTests
     {
+        [Fact]
+        public void Saves_All_Content()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            sut.SaveAll();
+
+            // Assert
+            MultipleContentBuilderMock.Verify(x => x.SaveAll(), Times.Once);
+        }
     }
 }
