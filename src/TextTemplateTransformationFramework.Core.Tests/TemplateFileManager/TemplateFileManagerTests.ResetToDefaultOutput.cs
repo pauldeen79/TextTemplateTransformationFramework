aa34eb5836b8fb69d5464ae3c1e0f4f19ab4068a
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-30 16:32:35 +0200

    Added unit test for ResetToDefaultOutput method of TemplateFileManager class

--- src/TextTemplateTransformationFramework.Core.Tests/TemplateFileManager/TemplateFileManagerTests.ResetToDefaultOutput.cs
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateFileManager/TemplateFileManagerTests.ResetToDefaultOutput.cs
@@ -4,5 +4,20 @@ public partial class TemplateFileManagerTests
 {
     public class ResetToDefaultOutput : TemplateFileManagerTests
     {
+        [Fact]
+        public void Resets_Builder_To_OriginalContent()
+        {
+            // Arrange
+            StringBuilder.Append("OriginalContent");
+            var sut = CreateSutWithRealMultipleContentBuilder();
+            sut.GenerationEnvironment.Append("Ignored");
+            sut.StartNewFile("MyFile.txt").Append("Also ignored");
+
+            // Act
+            sut.ResetToDefaultOutput();
+
+            // Assert
+            sut.GenerationEnvironment.ToString().Should().Be("OriginalContent");
+        }
     }
 }
