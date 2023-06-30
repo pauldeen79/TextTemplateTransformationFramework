134805ad93dee84fef7c39bb564a6ed94e472165
Paul Deen <paul.deen@dutchworkz.nl>
2023-06-30 17:02:38 +0200

    Added first tests for TemplateRenderer

---
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateRenderer/TemplateRendererTests.Base.cs
@@ -0,0 +1,10 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class TemplateRendererTests
+{
+    protected StringBuilder StringBuilder { get; } = new();
+    protected Mock<IMultipleContentBuilder> MultipleContentBuilderMock { get; } = new();
+    protected Mock<IMultipleContentBuilderContainer> MultipleContentBuilderContainerMock { get; } = new();
+
+    public override string ToString() => "ToString output";
+}
---
+++ src/TextTemplateTransformationFramework.Core.Tests/TemplateRenderer/TemplateRendererTests.Render.MultipleContentBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace TextTemplateTransformationFramework.Core.Tests;
+
+public partial class TemplateRendererTests
+{
+    public class Render_MultipleContentBuilder : TemplateRendererTests
+    {
+        [Fact]
+        public void Throws_On_Null_Template()
+        {
+            // Arrange
+            var sut = new TemplateRenderer();
+            object? template = null;
+            IMultipleContentBuilder? generationEnvironment = MultipleContentBuilderMock.Object;
+
+            // Act & Assert
+            sut.Invoking(x => x.Render(template!, generationEnvironment!))
+               .Should().Throw<ArgumentNullException>().WithParameterName(nameof(template));
+        }
+
+        [Fact]
+        public void Throws_On_Null_GenerationEnvironment()
+        {
+            // Arrange
+            var sut = new TemplateRenderer();
+            object? template = this;
+            IMultipleContentBuilder? generationEnvironment = null;
+
+            // Act & Assert
+            sut.Invoking(x => x.Render(template!, generationEnvironment!))
+               .Should().Throw<ArgumentNullException>().WithParameterName(nameof(generationEnvironment));
+        }
+    }
+}
