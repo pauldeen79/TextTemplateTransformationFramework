c0c99b13f41bcc7fa63ff3ad97d4aa70030455c0
Paul Deen <paul.deen@dutchworkz.nl>
2023-07-03 20:09:02 +0200

    Aligned NormalizeLineEndings implementation with CrossCutting.Common

--- src/TextTemplateTransformationFramework.Core/Extensions/StringExtensions.cs
+++ src/TextTemplateTransformationFramework.Core/Extensions/StringExtensions.cs
@@ -2,6 +2,12 @@
 
 public static class StringExtensions
 {
-    public static string NormalizeLineEndings(this string instance, TimeSpan? matchTimeout = null)
-        => Regex.Replace(instance, @"\r\n|\n\r|\n|\r", Environment.NewLine, RegexOptions.None, matchTimeout ?? TimeSpan.FromMilliseconds(200));
+    /// <summary>
+    /// Replaces line endings to the right format conform the current operating system
+    /// </summary>
+    /// <param name="instance">String instance to get the normalized line endings for</param>
+    /// <param name="matchTimeoutInMilliseconds">Time-out in milli secconds for the regular expression parsing</param>
+    /// <returns></returns>
+    public static string NormalizeLineEndings(this string instance, int matchTimeoutInMilliseconds = 500)
+        => Regex.Replace(instance, @"\r\n|\n\r|\n|\r", Environment.NewLine, RegexOptions.None, TimeSpan.FromMilliseconds(matchTimeoutInMilliseconds));
 }
