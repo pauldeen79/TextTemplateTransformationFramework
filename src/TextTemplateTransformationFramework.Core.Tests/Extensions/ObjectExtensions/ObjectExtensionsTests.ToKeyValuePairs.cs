020f1bc75a84b53f446fc5b323edc5f01f2ff61f
Paul Deen <paul.deen@dutchworkz.nl>
2023-07-09 10:51:31 +0200

    Added missing unit tests

---
+++ src/TextTemplateTransformationFramework.Core.Tests/Extensions/ObjectExtensions/ObjectExtensionsTests.ToKeyValuePairs.cs
@@ -0,0 +1,62 @@
+﻿namespace TemplateFramework.Core.Tests.Extensions;
+
+public class ObjectExtensionsTests
+{
+    public class ToKeyValuePairs
+    {
+        [Fact]
+        public void Returns_Empty_Dictionary_On_Null_Instance()
+        {
+            // Arrange
+            var input = (object?)null;
+
+            // Act
+            var result = input.ToKeyValuePairs();
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Returns_Dictionary_With_Provided_KeyValuePairs()
+        {
+            // Arrange
+            var input = new[]
+            {
+                new KeyValuePair<string, object?>("Item1", 1),
+                new KeyValuePair<string, object?>("Item2", "some value"),
+                new KeyValuePair<string, object?>("Item3", null),
+            };
+
+            // Act
+            var result = input.ToKeyValuePairs();
+
+            // Assert
+            result.Should().BeEquivalentTo(input);
+        }
+
+        [Fact]
+        public void Returns_Dictionary_With_Provided_Object_Instance()
+        {
+            // Arrange
+            var input = new
+            {
+                Item1 = 1,
+                Item2 = "some value",
+                Item3 = (object?) null,
+            };
+
+            // Act
+            var result = input.ToKeyValuePairs();
+
+            // Assert
+            result.Should().HaveCount(3);
+            result.ElementAt(0).Key.Should().Be("Item1");
+            result.ElementAt(0).Value.Should().Be(1);
+            result.ElementAt(1).Key.Should().Be("Item2");
+            result.ElementAt(1).Value.Should().Be("some value");
+            result.ElementAt(2).Key.Should().Be("Item3");
+            result.ElementAt(2).Value.Should().BeNull();
+        }
+    }
+}
---
+++ src/TextTemplateTransformationFramework.Core.Tests/Extensions/ServiceCollectionExtensions/ServiceCollectionExtensionsTests.AddTemplateFramework.cs
@@ -0,0 +1,19 @@
+﻿namespace TemplateFramework.Core.Tests.Extensions;
+
+public class ServiceCollectionExtensionsTests
+{
+    public class AddTemplateFramework
+    {
+        [Fact]
+        public void All_Dependencies_Can_Be_Resolved()
+        {
+            // Act
+            using var provider = new ServiceCollection()
+                .AddTemplateFramework()
+                .BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
+
+            // Assert
+            provider.Should().NotBeNull();
+        }
+    }
+}
